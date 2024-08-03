ecd6d79b7c91e5b28786efcbaf63156bc6dcdafe
MetalSnow <aymanetrend@gmail.com>
2024-08-03 14:14:41 +0800

    Set up files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Linked List Project
-
-This project implements a basic linked list data structure in JavaScript.
--- createNode.js
+++
@@ -1,6 +0,0 @@
-export default class node {
-  constructor(value = null, next = null) {
-    this.value = value;
-    this.next = next;
-  }
-}
