5c4ea41daf51fe27c623966cffca8ef1b26371c3
MetalSnow <aymanetrend@gmail.com>
2024-08-06 19:20:03 +0800

    fix: correct average in the analyzeArray function

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,7 +1,7 @@
 export default function analyzeArray(array) {
   const obj = {};
 
-  obj['average'] = array[Math.ceil((array.length - 1) / 2)];
+  obj['average'] = Math.ceil(array.reduce((a, b) => a + b) / array.length);
   obj['max'] = Math.max(...array);
   obj['min'] = Math.min(...array);
   obj['length'] = array.length;
